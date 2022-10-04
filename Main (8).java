721c35e221ae4f3d8f04dd050e4610908f9f4aaf
RamshaRauf <113073103+RamshaRauf@users.noreply.github.com>
2022-10-04 15:38:51 -0400

    Starts the game.

---
+++ Main (8).java
@@ -0,0 +1,13 @@
+
+/**
+  * Starts playing game 
+  */
+class Main {
+  public static void main(String[] args) {
+    
+    RobotInvasion firstInvasion = new RobotInvasion();
+    firstInvasion.setUp();
+    firstInvasion.playGame();
+    
+  }
+}
